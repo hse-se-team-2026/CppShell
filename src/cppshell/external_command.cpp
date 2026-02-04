ad51196a26f61bab00817679a354bf80e681210c
DimaRus05 <dimarus06122005@gmail.com>
2026-02-04 17:34:42 +0300

    Windows min/max macros fix

--- src/cppshell/external_command.cpp
+++ src/cppshell/external_command.cpp
@@ -7,6 +7,13 @@
 #include <thread>
 
 #ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
 #include <Windows.h>
 
 #include <vector>
